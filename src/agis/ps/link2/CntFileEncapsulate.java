09ab7b91933bb1b65e31c088a080a211ea63b024
mao qin <shingocat@gmail.com>
2016-10-18 17:20:52 +0800

    refactor some code

--- src/agis/ps/link2/CntFileEncapsulate.java
+++ src/agis/ps/link2/CntFileEncapsulate.java
@@ -13,8 +13,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import agis.ps.test.Contig;
-
 /**
  * Encapsulate the contig file;
  * lengths and seqs is reorder and rename to 
