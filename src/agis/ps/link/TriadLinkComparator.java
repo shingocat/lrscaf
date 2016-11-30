db897fe6d4e1d39dbd33462f2b0d90b334063b3e
mao qin <shingocat@gmail.com>
2016-11-30 09:56:23 +0800

    Fix a java bug by using collection sort method when the object is equal then it will return 0;

--- src/agis/ps/link/TriadLinkComparator.java
+++ src/agis/ps/link/TriadLinkComparator.java
@@ -13,9 +13,12 @@ public class TriadLinkComparator implements Comparator<TriadLink> {
 	@Override
 	public int compare(TriadLink o1, TriadLink o2) {
 		// TODO Auto-generated method stub
-		if(o1.getSupLinks() >= o2.getSupLinks())
+		if(o1.getSupLinks() > o2.getSupLinks())
 			return -1;
-		return 1;
+		else if(o1.getSupLinks() < o2.getSupLinks())
+			return 1;
+		else
+			return 0;
 	}
 
 }
